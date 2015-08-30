cfab160b1500f546f6bc16d278e67796c3c4e9d1
Mark Tomm <mark.tomm@martem.ee>
2015-08-30 18:23:07 +0300

    initial meteor setup.
    Package list:
    accounts-facebook          1.0.4  Login service for Facebook accounts
    accounts-password          1.1.1  Password support for accounts
    autopublish                1.0.3  Publish the entire database to all clients
    insecure                   1.0.3  Allow all database writes by default
    kadira:blaze-layout        2.0.1  Layout Manager for Blaze (works well with FlowRouter)
    kadira:flow-router         2.4.0  Carefully Designed Client Side Router for Meteor
    less                       1.0.14  The dynamic stylesheet language
    meteor-platform            1.2.2  Include a standard set of Meteor packages in your app
    nemo64:bootstrap           3.3.5_2  Highly configurable bootstrap integration.
    service-configuration      1.0.4  Manage the configuration for third-party services
    useraccounts:bootstrap     1.12.3  Accounts Templates styled for Twitter Bootstrap.
    useraccounts:flow-routing  1.12.3  UserAccounts package providing routes configuration capability via kadira:flow-router.
    
    Removed:
    autopublish
    insecure
    
    Enabled everyting in bootstrap conf -> client/lib/custom.bootstrap.json
    Social OAuth conf -> server/config_social.js

---
+++ server/config_social.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+ 
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '627443327397351',
+    secret: 'b862dfad3da3bf26e3a7478bd7628eb9'
+});
